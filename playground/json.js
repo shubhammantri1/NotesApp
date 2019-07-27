34d27e174a9652aa0b7c277382c57e0a88ba0fd6
shubhammantri1 <smantri600@gmail.com>
2019-07-27 19:14:00 +0530

    Some Updates

--- playground/json.js
+++ playground/json.js
@@ -1,15 +1,3 @@
-// var obj = {
-//   name: 'Andrew'
-// };
-// var stringObj = JSON.stringify(obj);
-// console.log(typeof stringObj);
-// console.log(stringObj);
-
-// var personString = '{"name": "Andrew","age": 25}';
-// var person = JSON.parse(personString);
-// console.log(typeof person);
-// console.log(person);
-
 const fs = require('fs');
 
 var originalNote = {
